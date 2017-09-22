ea261267916893542da32cacc3b573f53ababfcd
Andreas Remar <andreas.remar@gmail.com>
2017-09-22 23:29:19 +0200

    Don't use SDL2.SDL with using static. Use SDL_image to load PNG image.

--- Gfx.cs
+++ Gfx.cs
@@ -1,10 +1,10 @@
 using System;
-using static SDL2.SDL;
+using SDL2;
 
 class Gfx {
     private struct Tile {
-	public SDL_Rect src;
-	public SDL_Rect dest;
+	public SDL.SDL_Rect src;
+	public SDL.SDL_Rect dest;
 	public Tile(int tile, int x, int y) {
 	    src = MakeRect(tile * cellWidth, 0, cellWidth, cellHeight);
 	    dest = MakeRect(x * cellWidth, y * cellHeight,
@@ -35,42 +35,42 @@ class Gfx {
 	IntPtr Window = IntPtr.Zero;
 	IntPtr img = IntPtr.Zero;
 	
-	Window = SDL_CreateWindow ("Minesweeper!",
-				   SDL_WINDOWPOS_UNDEFINED,
-				   SDL_WINDOWPOS_UNDEFINED,
+	Window = SDL.SDL_CreateWindow ("Minesweeper!",
+				   SDL.SDL_WINDOWPOS_UNDEFINED,
+				   SDL.SDL_WINDOWPOS_UNDEFINED,
 				   cols * cellWidth, // Width
 				   rows * cellHeight, // Height
-				   SDL_WindowFlags.SDL_WINDOW_SHOWN);
+				   SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
 
 	Renderer =
-	    SDL_CreateRenderer(Window,
+	    SDL.SDL_CreateRenderer(Window,
 			       -1,
-			       SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+			       SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
 
-	SDL_SetRenderDrawColor (Renderer, 0xae, 0xFF, 0x00, 0xFF);
-	img = SDL_LoadBMP("gfx.bmp");
-	gfx = SDL_CreateTextureFromSurface(Renderer, img);
-	SDL_FreeSurface(img);
+	SDL.SDL_SetRenderDrawColor (Renderer, 0xae, 0xFF, 0x00, 0xFF);
+	img = SDL_image.IMG_Load("gfx.png");
+	gfx = SDL.SDL_CreateTextureFromSurface(Renderer, img);
+	SDL.SDL_FreeSurface(img);
     }
 
     public void Render() {
-	SDL_RenderClear (Renderer);
+	SDL.SDL_RenderClear (Renderer);
 	for(int y = 0;y < rows;y++) {
 	    for(int x = 0;x < cols;x++) {
-		SDL_RenderCopy(Renderer, gfx,
+		SDL.SDL_RenderCopy(Renderer, gfx,
 			       ref field[y,x].src,
 			       ref field[y,x].dest);
 	    }
 	}
-	SDL_RenderPresent (Renderer);
+	SDL.SDL_RenderPresent (Renderer);
     }
 
     public void SetTile(int tile, int x, int y) {
 	field[y,x].src.x = tile * cellWidth;
     }
 
-    private static SDL_Rect MakeRect(int x, int y, int w, int h) {
-	SDL_Rect rect = new SDL_Rect();
+    private static SDL.SDL_Rect MakeRect(int x, int y, int w, int h) {
+	SDL.SDL_Rect rect = new SDL.SDL_Rect();
 	rect.x = x;
 	rect.y = y;
 	rect.w = w;
--- Mine.cs
+++ Mine.cs
@@ -1,26 +1,27 @@
 using System;
-using static SDL2.SDL;
+using SDL2;
 
 class Mine {
     public static int Main() {
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
 	int rows = 15;
 	int cols = 15;
+	int mines = 30;
 
 	Gfx gfx = new Gfx(rows, cols);
 	Field field = new Field(gfx);
 
 	gfx.Init();
 
-	field.AddMines(20);
+	field.AddMines(mines);
 
-	SDL_Event Event;
+	SDL.SDL_Event Event;
 	bool Running = true;
 	while(Running) {
-	    while(SDL_PollEvent(out Event) != 0) {
-		if(Event.type == SDL_EventType.SDL_QUIT) {
+	    while(SDL.SDL_PollEvent(out Event) != 0) {
+		if(Event.type == SDL.SDL_EventType.SDL_QUIT) {
 		    Running = false;
-		} else if(Event.type == SDL_EventType.SDL_MOUSEBUTTONDOWN) {
+		} else if(Event.type == SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN) {
 		    int x = Event.button.x / 32;
 		    int y = Event.button.y / 32;
 		    field.Reveal(x, y);
@@ -29,7 +30,7 @@ class Mine {
 	    gfx.Render();
 	}
 
-	SDL_Quit();
+	SDL.SDL_Quit();
 
 	return 0;
     }
